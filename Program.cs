13cf6b79103fda117f4d374b18ad15a38291984d
SatinkaTiger <surferboy.rk@googlemail.com>
2023-03-26 17:40:48 +0200

    add contact email

--- Program.cs
+++ Program.cs
@@ -13,6 +13,8 @@
             /*Dynamisches Subnettieren IPv4
               Copyright (C) 2023  Rick Kummer
 
+              Contact Email: kummer_rick@gmx.de
+
               This program is free software; you can redistribute it and/or modify
               it under the terms of the GNU General Public License as published by
               the Free Software Foundation; either version 2 of the License, or
